2a6645d1822dd70cc7f78b97cf99b5a5654ad013
ghilardi.19320 <ghilardi.19320@LABI1-6PC12>
2023-02-27 10:54:06 +0100

    componenti iniziali

--- Colletta/Form1.Designer.cs
+++ Colletta/Form1.Designer.cs
@@ -29,33 +29,69 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(440, 12);
+            this.listView1.Location = new System.Drawing.Point(296, 28);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(348, 363);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Inserisci";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome utente:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(666, 419);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Colletta/Form1.cs
+++ Colletta/Form1.cs
@@ -17,6 +17,9 @@ namespace Colletta
             InitializeComponent();
         }
         public bool first = true;
+        Dictionary<string, double> accounts = new Dictionary<string, double>();
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
             if (first)//solo la prima volta che apro il programma
@@ -25,11 +28,22 @@ namespace Colletta
                 listView1.FullRowSelect = true;
                 first = false;
 
-                listView1.Columns.Add("UTENTI", 60);
+                listView1.Columns.Add("UTENTI", 200);
                 listView1.Columns.Add("QUOTA", 80);
-
+                
 
             }
+
+
+            accounts.Add("NICOLA", 0);
+            accounts.Add("FRED", 0);
+            accounts.Add("OLPROFESUR", 0);
+            accounts.Add("MARCO", 0);
+
+
+
+
+
         }
     }
 }
