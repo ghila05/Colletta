cd3730cd979039ded29dd4bbf023ab55b15145ac
ghila05 <nicolaghi05@gmail.com>
2023-03-04 15:42:51 +0100

    aggiunta, modifica, elimina, totale fatte

--- Colletta/Form1.Designer.cs
+++ Colletta/Form1.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -44,25 +46,17 @@
             this.listView1.Location = new System.Drawing.Point(395, 34);
             this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(463, 446);
+            this.listView1.Size = new System.Drawing.Size(463, 413);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(119, 34);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 24);
-            this.textBox1.TabIndex = 1;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(20, 431);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 50);
+            this.button1.Size = new System.Drawing.Size(104, 50);
             this.button1.TabIndex = 2;
             this.button1.Text = "Inserisci";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,7 +74,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(125, 121);
+            this.textBox2.Location = new System.Drawing.Point(119, 119);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 4;
@@ -96,25 +90,60 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(178, 431);
+            this.button2.Location = new System.Drawing.Point(131, 431);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 49);
+            this.button2.Size = new System.Drawing.Size(105, 49);
             this.button2.TabIndex = 6;
             this.button2.Text = "Elimina";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "NICOLA",
+            "FRED",
+            "OLPROFESUR",
+            "MARCO"});
+            this.comboBox1.Location = new System.Drawing.Point(119, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(730, 461);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 36);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "TOT: 0 ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(242, 432);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 49);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Modifica";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(888, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
@@ -128,12 +157,14 @@
         #endregion
 
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Colletta/Form1.cs
+++ Colletta/Form1.cs
@@ -17,8 +17,8 @@ namespace Colletta
             InitializeComponent();
         }
         public bool first = true;
-        Dictionary<string, string> accounts = new Dictionary<string, string>();
-
+        Dictionary<string, double> accounts = new Dictionary<string, double>();
+        double tot;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -29,86 +29,115 @@ namespace Colletta
                 first = false;
 
                 listView1.Columns.Add("UTENTI", 200);
-                listView1.Columns.Add("QUOTA", 80);
-                listView1.Columns.Add("TOT");
+                listView1.Columns.Add("QUOTA", 100);
+
 
             }
 
 
-            accounts.Add("NICOLA", "");
-            accounts.Add("FRED", "");
-            accounts.Add("OLPROFESUR", "");
-            accounts.Add("MARCO", "");
+            accounts.Add("NICOLA", 0);
+            accounts.Add("FRED", 0);
+            accounts.Add("OLPROFESUR", 0);
+            accounts.Add("MARCO", 0);
             
 
-            Stampa();
 
 
         }
 
-        public void Stampa()
+        public void Add()
         {
+            ListViewItem item = new ListViewItem(comboBox1.Text);
+            item.SubItems.Add(textBox2.Text);
+            listView1.Items.Add(item);
+            PrintTot();   
+        }
 
-
-            listView1.Items.Clear();
-            foreach (KeyValuePair<string, string> kvp in accounts)
+        public void PrintTot()
+        {
+            tot = 0;
+            foreach (KeyValuePair<string, double> kvp in accounts)
             {
-                ListViewItem item = new ListViewItem(kvp.Key);
-                item.SubItems.Add(kvp.Value);
-                //item.SubItems.Add()
-                listView1.Items.Add(item);
-
+                //MessageBox.Show(Convert.ToString(kvp.Value));
+                tot += kvp.Value;
             }
 
-
-           
+            label3.Text = ("TOT: "+Convert.ToString(tot));
         }
-        /*
-        public string Somma()
-        {
-        
-
-
-
-        }
-       */
-
 
         private void button1_Click(object sender, EventArgs e) //button add
         {
-            string ut;
+            string ut="";
             string quota="";
-
-
-            ut = textBox1.Text;
-            
+            ut = comboBox1.Text;
             quota += textBox2.Text;
-            quota += ";";
-            accounts[ut.ToUpper()] += quota;
-            Stampa();
-        
+            accounts[ut] += Double.Parse(quota);
+
+            Add();
+
+
         }
+
 
         private void button2_Click(object sender, EventArgs e)//button Elimina
         {
             string tot="";
-            //acquisisco vaalori del dizionario della riga selezionata
             string ut = listView1.SelectedItems[0].SubItems[0].Text;
             string quote = listView1.SelectedItems[0].SubItems[1].Text;
-            MessageBox.Show("quote" + quote);
 
-            string[] q = new string [100];
-            q = quote.Split(';');
-      
-            //ristampo tutte le quote tranne l'ultima
-            for(int i=0; i < q.Length - 2; i++)
+            accounts[ut] -= Double.Parse(quote);
+
+
+            if (listView1.SelectedItems.Count > 0)
             {
-                tot = tot + q[i] + ";";
+                listView1.Items.Remove(listView1.SelectedItems[0]);
             }
- 
-            accounts[ut] = tot;
-            Stampa();
 
+            PrintTot();
+
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//modifica
+        {
+            string ut = listView1.SelectedItems[0].SubItems[0].Text;
+            double quote = Double.Parse(listView1.SelectedItems[0].SubItems[1].Text);
+            double newq = 0;
+            newq = Double.Parse(textBox2.Text);
+
+
+            quote -= newq;
+
+            MessageBox.Show(Convert.ToString(quote));
+
+            if (quote < 0)
+            {
+                accounts[ut] += (-quote);
+            }
+            else
+            {
+                accounts[ut] -= quote;
+            }
+
+
+            listView1.SelectedItems[0].SubItems[1].Text = textBox2.Text;
+
+
+
+
+            PrintTot();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string ut = listView1.SelectedItems[0].SubItems[0].Text;
+            string quote = listView1.SelectedItems[0].SubItems[1].Text;
+            comboBox1.Text = ut;
+            textBox2.Text = quote;
         }
     }
 }
